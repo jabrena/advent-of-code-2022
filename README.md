f643c644270e998bfdca1721de387ff5e0ffa2a8
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-12-01 09:30:01 +0100

    Adding initial solution for AOC2022, Day 1

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Solutions for advent-of-code-2022
 
 ```
+mvn prettier:write
 mvn compile exec:java -Dexec.mainClass="info.aoc.jab.Problem1"
 ```
 
--- src/main/java/info/aoc/jab/Problem1.java
+++ src/main/java/info/aoc/jab/Problem1.java
@@ -3,13 +3,13 @@ package info.aoc.jab;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -20,9 +20,8 @@ import java.util.function.Function;
  */
 public class Problem1 {
 
-    public static void main( String[] args ) {
-
-        Function<String, List<String>> loadFile = (fileName) -> {
+    public static void main(String[] args) {
+        Function<String, List<String>> loadFile = fileName -> {
             try {
                 ClassLoader classloader = Thread.currentThread().getContextClassLoader();
                 File file = new File(classloader.getResource(fileName).toURI());
@@ -33,31 +32,34 @@ public class Problem1 {
         };
 
         //TODO Refactor with a better Java Stream usage.
-        Function<List<String>, List<Long>> group = (list) -> {
+        Function<List<String>, List<Long>> group = list -> {
             AtomicLong counter = new AtomicLong(0);
             List<Long> groups = new ArrayList<>();
-            list.stream()
-                    .forEach(x -> {
-                        if(x.length() != 0) {
-                            counter.addAndGet(Long.parseLong(x));
-                        } else {
-                            groups.add(counter.get());
-                            counter.set(0);
-                        }
-                    });
+            list
+                .stream()
+                .forEach(x -> {
+                    if (x.length() != 0) {
+                        counter.addAndGet(Long.parseLong(x));
+                    } else {
+                        groups.add(counter.get());
+                        counter.set(0);
+                    }
+                });
 
             return groups;
         };
 
+        // @formatter:off
         Function<List<Long>, Long> top1 = param -> param.stream()
                 .sorted(Comparator.reverseOrder())
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("Katakroker"));
+                .limit(1)
+                .reduce(0L, Long::sum);
 
-        Function<List<Long>, Long> top3 = param -> param.stream()
+        Function<List<Long>, Long> top3 = (param) -> param.stream()
                 .sorted(Comparator.reverseOrder())
                 .limit(3)
-                .reduce(0L, (a, b) -> a + b);
+                .reduce(0L, Long::sum);
+        // @formatter:on
 
         Consumer<String> showSolution1 = param -> {
             Long result = loadFile.andThen(group).andThen(top1).apply(param);
@@ -69,7 +71,7 @@ public class Problem1 {
             System.out.println("Result2: " + result);
         };
 
-        System.out.println( "Day 1: Calorie Counting" );
+        System.out.println("Day 1: Calorie Counting");
 
         System.out.println("Sample");
         String param = "problem1-input-sample.txt";
