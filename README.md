29055b7e2d2e2875456af3fbb8d641425b631e5d
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-12-01 18:08:55 +0100

    Adding initial solution for AOC2022, Day 1 (#1)

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # advent-of-code-2022
+
 Solutions for advent-of-code-2022
+
+[![CI](https://github.com/jabrena/advent-of-code-2022/actions/workflows/ci.yaml/badge.svg)](https://github.com/jabrena/advent-of-code-2022/actions/workflows/ci.yaml)
+
+```
+mvn prettier:write
+mvn compile exec:java -Dexec.mainClass="info.aoc.jab.Problem1"
+```
+
+## References
+
+- https://adventofcode.com/
---
+++ src/main/java/info/aoc/jab/Utils.java
@@ -0,0 +1,40 @@
+package info.aoc.jab;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Function;
+
+public class Utils {
+
+    /**
+     * Given a file name stored in resources
+     * Return the file as a list of String
+     */
+    public static List<String> loadFileToList(String fileName) {
+        try {
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            File file = new File(classloader.getResource(fileName).toURI());
+            return Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Given a file name stored in resources
+     * Return the file as a String
+     */
+    public static String readFileToString(String fileName) {
+        try {
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            return Files.readString(Path.of(classloader.getResource(fileName).getPath()));
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
