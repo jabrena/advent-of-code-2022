647f63830a4de66670018a3a23f4abcf811e01e3
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-12-02 13:01:08 +0100

    Day refactoring

--- src/main/java/jab/aoc/day1/Day1.java
+++ src/main/java/jab/aoc/day1/Day1.java
@@ -3,7 +3,6 @@ package jab.aoc.day1;
 import jab.aoc.Utils;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.function.BiFunction;
 import java.util.stream.LongStream;
 
 /**
@@ -16,23 +15,24 @@ public class Day1 {
     private static final String GROUP_SEPARATOR = System.lineSeparator() + System.lineSeparator();
     private static final String LINE_SEPARATOR = System.lineSeparator();
 
-    private BiFunction<String, Integer, Long> processData = (file, limit) ->
-        Arrays
-            .stream(file.split(GROUP_SEPARATOR))
+    private Long processData(String fileName, Integer limit) {
+        return Arrays
+            .stream(fileName.split(GROUP_SEPARATOR))
             .map(group -> Arrays.stream(group.split(LINE_SEPARATOR)))
             .flatMapToLong(item -> LongStream.of(item.mapToLong(Long::parseLong).sum()))
             .boxed()
             .sorted(Comparator.reverseOrder())
             .limit(limit)
             .reduce(0L, Long::sum);
+    }
 
     public Long getPart1Result(String fileName) {
         var fileLoaded = Utils.readFileToString(fileName);
-        return processData.apply(fileLoaded, 1);
+        return this.processData(fileLoaded, 1);
     }
 
     public Long getPart2Result(String fileName) {
         var fileLoaded = Utils.readFileToString(fileName);
-        return processData.apply(fileLoaded, 3);
+        return this.processData(fileLoaded, 3);
     }
 }
