1ff7d201bb1dabec0beba51b8a66b1f5fe4ce858
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-12-02 13:08:46 +0100

    Day2 refactoring

--- src/main/java/jab/aoc/day2/Day2.java
+++ src/main/java/jab/aoc/day2/Day2.java
@@ -12,19 +12,21 @@ public class Day2 {
 
     private static final String SEPARATOR = " ";
 
-    private Stream<String[]> loadData(String fileName) {
-        return Utils.loadFileToList(fileName).stream().map(line -> line.split(SEPARATOR));
-    }
-
     public Integer getPart1Result(String fileName) {
-        return loadData(fileName)
+        return Utils
+            .loadFileToList(fileName)
+            .stream()
+            .map(line -> line.split(SEPARATOR))
             .map(arr -> new Game1(Column1.fromValue(arr[0]), Column2.fromValue(arr[1])))
             .map(Game1::getScore)
             .reduce(0, Integer::sum);
     }
 
     public Integer getPart2Result(String fileName) {
-        return loadData(fileName)
+        return Utils
+            .loadFileToList(fileName)
+            .stream()
+            .map(line -> line.split(SEPARATOR))
             .map(arr -> new Game2(Column1.fromValue(arr[0]), Column2.fromValue(arr[1])))
             .map(Game2::getScore)
             .reduce(0, Integer::sum);
