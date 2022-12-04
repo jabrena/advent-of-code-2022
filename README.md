b8431cfba354101e915f5e99daecd77f1a6e82f7
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-12-04 14:23:02 +0100

    Adding initial test for Day 4 (#4)
    * Adding initial test for Day 4
    
    * Adding initial solution for Day 4
    
    * Refactoring for Day 4
    
    * Refactoring for Day 4
    
    * Refactoring for Day 4

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Solutions for advent-of-code-2022
 
 [![CI](https://github.com/jabrena/advent-of-code-2022/actions/workflows/ci.yaml/badge.svg)](https://github.com/jabrena/advent-of-code-2022/actions/workflows/ci.yaml)
 
+- [Day 1: Calorie Counting](src/main/java/jab/aoc/day1)
+- [Day 2: Rock Paper Scissors](src/main/java/jab/aoc/day2)
+- [Day 3: Rucksack Reorganization](src/main/java/jab/aoc/day3)
+- [Day 4: Camp Cleanup](src/main/java/jab/aoc/day4)
+
 ```
 mvn prettier:write
 mvn test
---
+++ src/main/java/jab/aoc/day4/Day4.java
@@ -0,0 +1,97 @@
+package jab.aoc.day4;
+
+import jab.aoc.Utils;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class Day4 {
+
+    //TODO Refactor
+    //From: https://www.geeksforgeeks.org/find-whether-an-array-is-subset-of-another-array-set-1/
+    private boolean isSubset(Integer[] arr1, Integer[] arr2) {
+        int m = arr1.length;
+        int n = arr2.length;
+        int i = 0;
+        int j = 0;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                if (arr2[i] == arr1[j]) {
+                    break;
+                }
+            }
+            if (j == m) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // @formatter:off
+    private Function<String, Integer[]> toIntArr = arr -> {
+        String[] parts1 = arr.split("-");
+        var list1 = IntStream
+                .rangeClosed(Integer.parseInt(parts1[0]), Integer.parseInt(parts1[1]))
+                .boxed()
+                .toList();
+
+        //TODO Refactor; Coupled to the method to detect subsets
+        Integer[] parts1Arr = new Integer[list1.size()];
+        parts1Arr = list1.toArray(parts1Arr);
+
+        return parts1Arr;
+    };
+
+    // @formatter:on
+
+    record Tuple(Integer[] part1, Integer[] part2) {}
+
+    Function<String, Tuple> toTuple = param -> {
+        String[] arr = param.split(",");
+        return new Tuple(toIntArr.apply(arr[0]), toIntArr.apply(arr[1]));
+    };
+
+    public Long getPart1Result(String fileName) {
+        Function<Tuple, Boolean> areSubset = tuple -> {
+            if (isSubset(tuple.part1(), tuple.part2()) || isSubset(tuple.part2(), tuple.part1())) {
+                return true;
+            }
+            return false;
+        };
+
+        // @formatter:off
+        return Utils.loadFileToList(fileName)
+                .stream()
+                .map(toTuple)
+                .map(areSubset)
+                .filter(value -> value)
+                .count();
+        // @formatter:on
+    }
+
+    public Long getPart2Result(String fileName) {
+        /**
+         * Detect if 2 Arrays Overlaps
+         */
+        BiFunction<Integer[], Integer[], Boolean> overlap = (arr1, arr2) -> {
+            HashSet<Integer> set2 = new HashSet<>();
+            set2.addAll(Arrays.asList(arr1));
+            set2.retainAll(Arrays.asList(arr2));
+
+            if (set2.size() > 0) {
+                return true;
+            }
+            return false;
+        };
+
+        return Utils
+            .loadFileToList(fileName)
+            .stream()
+            .map(toTuple)
+            .map(tuple -> overlap.apply(tuple.part1(), tuple.part2()))
+            .filter(value -> value)
+            .count();
+    }
+}
