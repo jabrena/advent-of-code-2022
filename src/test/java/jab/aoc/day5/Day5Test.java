51d8900d77934452e1ce86c14bfcff783b4d9cba
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-12-05 15:11:54 +0100

    Feature/day5 (#7)
    * Adding test for Day5 problem
    
    * Adding solution for Day 5

---
+++ src/test/java/jab/aoc/day5/Day5Test.java
@@ -0,0 +1,60 @@
+package jab.aoc.day5;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+import org.junit.jupiter.api.Test;
+
+public class Day5Test {
+
+    @Test
+    public void given_sampleData_when_execute_day5_getPart1Solution_then_expected_result() {
+        //Given
+        String fileName = "day5/input-sample.txt";
+
+        //When
+        Day5 day5 = new Day5();
+        var result = day5.getPart1Result(fileName);
+
+        //Then
+        then(result).isEqualTo("CMZ");
+    }
+
+    @Test
+    public void given_data_when_execute_day5_getPart1Solution_then_expected_result() {
+        //Given
+        String fileName = "day5/input.txt";
+
+        //When
+        Day5 day5 = new Day5();
+        var result = day5.getPart1Result(fileName);
+
+        //Then
+        then(result).isEqualTo("TLFGBZHCN");
+    }
+
+    @Test
+    public void given_sampleData_when_execute_day5_getPart2Solution_then_expected_result() {
+        //Given
+        String fileName = "day5/input-sample.txt";
+
+        //When
+        Day5 day5 = new Day5();
+        var result = day5.getPart2Result(fileName);
+
+        //Then
+        then(result).isEqualTo("MCD");
+    }
+
+    @Test
+    public void given_data_when_execute_day5_getPart2Solution_then_expected_result() {
+        //Given
+        String fileName = "day5/input.txt";
+
+        //When
+        Day5 day5 = new Day5();
+        var result = day5.getPart2Result(fileName);
+
+        //Then
+        then(result).isEqualTo("QRQFHFWCL");
+    }
+}
