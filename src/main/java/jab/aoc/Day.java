fae9f08a84e70451e8bc6a760adc9341ebe084de
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-12-06 11:24:39 +0100

    Fixing code smells detected by Sonar

--- src/main/java/jab/aoc/Day.java
+++ src/main/java/jab/aoc/Day.java
@@ -1,5 +1,12 @@
 package jab.aoc;
 
+/**
+ * Advent of Code define a set of problems every December.
+ * Every day, the website publish 2 problems to solve.
+ * This interface define the problems to solve per Day.
+ *
+ * @param <T>
+ */
 public interface Day<T> {
     T getPart1Result(String fileName);
 
