515f70fb5838f0b94fc8f7508a3322602158596f
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-12-11 06:15:44 +0100

    Uploading solution for problem 10

--- src/main/java/jab/aoc/day10/Day10.java
+++ src/main/java/jab/aoc/day10/Day10.java
@@ -1,16 +1,67 @@
 package jab.aoc.day10;
 
-import jab.aoc.Day;
+import jab.aoc.Utils;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Day10 implements Day<Integer> {
+public class Day10 {
 
-    @Override
-    public Integer getPart1Result(String fileName) {
-        return 0;
+    record Tuple(Integer cycle, Integer value) {}
+
+    public static List<Tuple> process(List<String> data) {
+        int value = 1;
+        int cycle = 1;
+        List<Tuple> list = new ArrayList<>();
+        for (String line : data) {
+            String[] split = line.split(" ");
+            if (split[0].equals("noop")) {
+                list.add(new Tuple(cycle, value));
+                cycle += 1;
+            } else {
+                list.add(new Tuple(cycle, value));
+                cycle += 1;
+                list.add(new Tuple(cycle, value));
+                cycle += 1;
+                value += Integer.parseInt(split[1]);
+            }
+        }
+        return list;
     }
 
-    @Override
-    public Integer getPart2Result(String fileName) {
-        return 0;
+    public Integer getPart1Result(String fileName) {
+
+        var list = Utils.readFileToList(fileName);
+
+        int[] cycles = {20, 60, 100, 140, 180, 220};
+        int score = 0;
+        var values = process(list);
+
+        for(var c : cycles) {
+            Tuple result = values.stream()
+                    .filter(v -> v.cycle == c)
+                    .findFirst()
+                    .orElseThrow();
+            score += c * result.value;
+        }
+        return score;
+    }
+
+    public String getPart2Result(String fileName) {
+
+        var list = Utils.readFileToList(fileName);
+        var values = process(list);
+
+        List<String> screen = new ArrayList<>();
+        screen.add("");
+        for (var i : values) {
+            if ((i.cycle - 1) % 40 == 0) {
+                screen.add("");
+            }
+            screen.set(screen.size() - 1, screen.get(screen.size() - 1) +
+                    (((i.cycle - 1) % 40 == i.value - 1
+                            || (i.cycle - 1) % 40 == i.value
+                            || (i.cycle - 1) % 40 == i.value + 1) ? "#" : "."));
+        }
+        return String.join("\n", screen);
     }
 }
--- src/test/java/jab/aoc/day10/Day10Test.java
+++ src/test/java/jab/aoc/day10/Day10Test.java
@@ -16,6 +16,67 @@ class Day10Test {
         var result = day10.getPart1Result(fileName);
 
         //Then
-        then(result).isEqualTo(10);
+        then(result).isEqualTo(13140);
+    }
+
+    @Test
+    void given_data_when_execute_day10_getPart1Solution_then_expected_result() {
+        //Given
+        String fileName = "day10/input.txt";
+
+        //When
+        Day10 day10 = new Day10();
+        var result = day10.getPart1Result(fileName);
+
+        //Then
+        then(result).isEqualTo(12540);
+    }
+
+    @Test
+    void given_sampleData_when_execute_day10_getPart2Solution_then_expected_result() {
+        //Given
+        String fileName = "day10/input-sample.txt";
+
+        //When
+        Day10 day10 = new Day10();
+        var result = day10.getPart2Result(fileName);
+
+        var expectedResult =
+"""
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+""";
+
+        //Then
+        then(result).isEqualTo(expectedResult);
+    }
+
+    //FECZELHE
+    @Test
+    void given_data_when_execute_day10_getPart2Solution_then_expected_result() {
+        //Given
+        String fileName = "day10/input.txt";
+
+        //When
+        Day10 day10 = new Day10();
+        var result = day10.getPart2Result(fileName);
+
+        //Then
+        var expectedResult =
+"""
+####.####..##..####.####.#....#..#.####.
+#....#....#..#....#.#....#....#..#.#....
+###..###..#......#..###..#....####.###..
+#....#....#.....#...#....#....#..#.#....
+#....#....#..#.#....#....#....#..#.#....
+#....####..##..####.####.####.#..#.####.
+""".trim();
+
+
+        then(result).isEqualTo(expectedResult);
     }
 }
