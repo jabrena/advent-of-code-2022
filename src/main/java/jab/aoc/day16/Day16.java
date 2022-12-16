909671308640dc51c022f5b579c239e2bd5af10d
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-12-16 06:52:03 +0100

    Adding initial test for Day16

---
+++ src/main/java/jab/aoc/day16/Day16.java
@@ -0,0 +1,16 @@
+package jab.aoc.day16;
+
+import jab.aoc.Day;
+
+public class Day16 implements Day<Long> {
+
+    @Override
+    public Long getPart1Result(String fileName) {
+        return null;
+    }
+
+    @Override
+    public Long getPart2Result(String fileName) {
+        return null;
+    }
+}
